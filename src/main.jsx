d7603221ee8c9c3606648b52943e10f9267186e7
Wolter Gray <114577932+MrSuperMn@users.noreply.github.com>
2023-09-29 15:25:47 +0200

    correct main

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App/App'
-import './index.scss'
+import React from "react";
+import App from "./components/App/App";
+import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
